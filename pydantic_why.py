75f1797dc4744ca7d65f0b49122d4911980ccf9f
Madhavi <dixitmadhavi12@gmail.com>
2026-07-18 22:23:10 +0530

    Add files via upload

---
+++ pydantic_why.py
@@ -0,0 +1,12 @@
+def insert_patient_data(name, age):
+    print(name),
+    print(age),
+    print("interested into database")
+
+ 
+    insert_patient_data('madhavi','twenty')
+
+    # biggest problem  with this code is that we are not validating the data before inserting any database (biggest failure)
+
+    
+
